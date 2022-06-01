b77c4b586a9e07702d0a25d1dce7ebfcdb4301db
FIRST_AOUCHICHE LAST_NAIM <naim.aouchiche@sncf.fr>
2022-06-01 16:44:07 +0200

    last commit

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -4,8 +4,15 @@ myAvatar.addEventListener("click", function(){
     myAvatar.src = ("./image/logo-wcs.png");
 })
 
+let name = document.getElementsByClassName("#name"); 
+  name.addEventListener("click", changeName);
 
+  
 
-const val = prompt ("Enter your name");
-const myName = document.getElementById ("#name");
+  
+  
+
+// const val = prompt ("Enter your name");
+// const myName = document.getElementById ("#name");
+// myName.
 
