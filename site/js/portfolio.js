959d069c9c40ee890863f7e390fc433cad231289
FIRST_AOUCHICHE LAST_NAIM <naim.aouchiche@sncf.fr>
2022-06-02 10:28:48 +0200

    add_js3

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -3,6 +3,14 @@ const myAvatar = document.querySelector(".avatar-bis");
 myAvatar.addEventListener("click", function(){
     myAvatar.src = ("./image/logo-wcs.png");
 })
+
+const myButton1 = document.querySelector(".button1");
+let myColor = document.querySelector(".description");
+myButton1.addEventListener("click", () =>{
+  let myPrompt1 = prompt ("Enter a new color");
+  myColor.style.backgroundColor = myPrompt1;
+})
+
 const myButton = document.querySelector(".button1");
 let myName = document.querySelector("#name"); 
   myButton.addEventListener("click", () =>{
@@ -10,12 +18,4 @@ let myName = document.querySelector("#name");
       myName.innerText = myPrompt;
   });
 
-  
-
-  
-  
-
-// const val = prompt ("Enter your name");
-// const myName = document.getElementById ("#name");
-// myName.
 
